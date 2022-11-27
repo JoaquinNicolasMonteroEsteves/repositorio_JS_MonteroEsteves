cb5ecda0fa38e5669125c97a1a1d9e937abb56e7
Joaquin Nicolas Montero Esteves <jnmontero@unlam.edu.ar>
2022-11-26 22:21:46 -0300

    Muchitos cambios

--- archivosJS/index.js
+++ archivosJS/index.js
@@ -1,20 +1,30 @@
+const container = document.getElementById("container")
+
 function cardPokemon (pokemon) {
-    return `<div class="card">
-                <div class="slide slide1 ${pokemon.bg}" id="card${pokemon.id}">
-                    <div class="card-nombre"><h2>${pokemon.nombre}</h2></div>
-                    <div class="card-imagen"><img id="imagen-pokemon" src=${pokemon.imagen}></div>
-                </div>
-                <div class="slide slide2">
-                    <div class="caracteristica">
-                        <ul>
-                            <li>Tipo: ${pokemon.tipo}</li>
-                            <li>Ataque favorito: ${pokemon.ataqueFavorito}</li>
-                            <li>Evolución/es: ${pokemon.evolucion}</li>
-                        </ul>
+    return `<label>
+                <input type="checkbox" class="card-pokemon" name="pokecard" value="${pokemon.id}"/>
+                <div class="card">
+                    <div class="slide slide0">
+                        <button type="button" class="botonesCarrito"><img src="./Imagenes/pokebola.png" class="imagenCarrito"></button>
+                    </div>
+                    <div class="slide slide1 ${pokemon.bg}" id="card${pokemon.id}">
+                        <div class="card-nombre"><h2>${pokemon.nombre}</h2></div>
+                        <div class="card-imagen"><img id="imagen-pokemon" src=${pokemon.imagen}></div>
+                    </div>
+                    <div class="slide slide2">
+                        <div class="caracteristica">
+                            <ul>
+                                <li>Tipo: ${pokemon.tipo}</li>
+                                <li>Ataque favorito: ${pokemon.ataqueFavorito}</li>
+                                <li>Evolución/es: ${pokemon.evolucion}</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>`
+            </label>`
 }
+
+
 
 function cargarPokemones (array) {
     let contenido = ""
@@ -26,9 +36,33 @@ function cargarPokemones (array) {
         }
 }
 cargarPokemones(pokemones)
- function bla () {
-    return "Hola como\nestas?"
- }
+
+function mostrarSeleccionado () {
+    const seleccionado = document.querySelector("input:checked")
+    if (seleccionado.length >= 1 && seleccionado.length < 6) {
+        let a = pokemones.find(encontrado => encontrado.id === parseInt(seleccionado.value))
+        equipo.push(a)
+        return equipo
+    } else if (seleccionado.length < 1) {
+        console.log("Por favor, seleccione uno o más pokemones")
+    } else if (seleccionado.length > 5) {
+        console.log("No puede seleccionar más de 5 pokemones")
+    }
+}
+
+// function seleccionados () {
+//     let cartas = document.querySelectorAll('input[name="pokecard"]:checked')
+//     if (cartas.length < 6) {
+//         cartas.forEach(elegido => {
+//             let encontrado = pokemones.find(a = a.id === elegido.value)
+//             equipo.push(encontrado)
+//             console.table(equipo)
+//         })
+//     } else {
+//         alert("No puede seleccionar más de 5 pokemones!")
+//     }
+// }
+// seleccionados()
 
 
 
