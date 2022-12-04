ca76e191097151f41715bb172ac28ae5cbdfc6f7
Joaco <joaquinnme@outlook.com>
2022-12-04 18:17:36 -0300

    Muchos cambios

--- archivosJS/index.js
+++ archivosJS/index.js
@@ -6,43 +6,43 @@ const ncontainer = document.getElementById("nBuscados")
 const ttitulo = document.getElementById("tBuscadosTitulo")
 const tcontainer = document.getElementById("tBuscados")
 const btnBuscados = document.getElementById("btnBuscados")
+const btnStorage = document.getElementById("btnStorage")
 const btnLimpiar = document.getElementById("btnLimpiar")
 
 
 let cardPokemon = (pokemon) => {
-    return `<label>
-                <input type="checkbox" class="card-pokemon" name="pokecard" value="${pokemon.id}"/>
-                <div class="card">
-                    <div class="slide slide0">
+    return `<div class="card">
+                <div class="slide slide0">
+                    <div class="costados izquierda">
+                        <img src="./Imagenes/pokebolaVacia.png" class="imagenCarrito">
+                    </div>
+                    <div class="costados derecha">
                         <button type="button" class="botonesEquipo" id="${pokemon.id}"><img src="./Imagenes/pokebola.png" class="imagenCarrito"></button>
                     </div>
-                    <div class="slide slide1 ${pokemon.bTipo}" id="card${pokemon.id}">
-                        <div class="card-nombre"><h2>${capitalize(pokemon.nombre)}</h2></div>
-                        <div class="card-imagen"><img id="imagen-pokemon" src=${pokemon.imagen}></div>
-                    </div>
-                    <div class="slide slide2">
-                        <div class="caracteristica">
-                            <ul>
-                                <li>Tipo: ${pokemon.tipo}</li>
-                                <li>Ataque favorito: ${pokemon.ataqueFavorito}</li>
-                                <li>Evolución/es: ${pokemon.evolucion}</li>
-                            </ul>
-                        </div>
+                </div>
+                <div class="slide slide1 ${pokemon.bTipo}" id="card${pokemon.id}">
+                    <div class="card-nombre"><h2>${capitalize(pokemon.nombre)}</h2></div>
+                    <img id="imagen-pokemon" src=${pokemon.imagen}>
+                </div>                    
+                <div class="slide slide2">
+                    <div class="caracteristica">
+                        <ul>
+                            <li>Tipo: ${pokemon.tipo}</li>
+                            <li>Ataque favorito: ${pokemon.ataqueFavorito}</li>
+                            <li>Evolución/es: ${pokemon.evolucion}</li>
+                        </ul>
                     </div>
                 </div>
-            </label>`
+            </div>`
 }
 
 let cardPokemonBuscado = (pokemon) => {
-    return `<label>
-                <input type="checkbox" class="card-pokemon" name="pokecard" value="${pokemon.id}"/>
-                <div class="card ">
-                    <div class="slide slide1 ${pokemon.bTipo} buscado" id="card${pokemon.id}">
-                        <div class="card-nombre"><h2>${capitalize(pokemon.nombre)}</h2></div>
-                        <div class="card-imagen"><img id="imagen-pokemon" src=${pokemon.imagen}></div>
-                    </div>
-                </div>
-            </label>`
+    return `<div class="card resultado">
+                <div class="slide slide1 ${pokemon.bTipo}" id="card${pokemon.id}">
+                    <div class="card-nombre"><h2>${capitalize(pokemon.nombre)}</h2></div>
+                    <img id="imagen-pokemon" src=${pokemon.imagen}>
+                </div>    
+            </div>`
 }
 
 let capitalize = (x) => {
@@ -69,7 +69,7 @@ let cargarPokemonesBuscados = (array, banner, elemento, filtro) => {
     let contenido = ""
         if(array.length > 0) {
             array.forEach (pokemon => {
-                titulo = `<h2>POKEMONES BUSCADOS POR ${filtro}</h2>`
+                titulo = `<h2>POR ${filtro}</h2>`
                 contenido += cardPokemonBuscado(pokemon)
             })
             banner.innerHTML = titulo
@@ -80,25 +80,10 @@ let cargarPokemonesBuscados = (array, banner, elemento, filtro) => {
 let alertaBuscador = (a) => {
     switch(a) {
     case 1:
-        Swal.fire({
-            position: 'center',
-            imageUrl: './Imagenes/VoltorbEnojado.png',
-            title: 'No se han hayado coincidencias',
-            text: 'Intente nuevamente, por favor',
-            showConfirmButton: false,
-            timer: 2500,
-        })
+        swal("No se han hayado coincidencias", "Intente nuevamente, por favor", "./Imagenes/VoltorbEnojado.png", {buttons: false, timer: 2500})
         break
     case 2:
-        Swal.fire({
-            position: 'center',
-            imageUrl: './Imagenes/JigglypuffEnojado.png',
-            imageWidth: 300,
-            title: 'Búsqueda fallida',
-            text: 'Elija un filtro, por favor',
-            showConfirmButton: false,
-            timer: 2500,
-        })
+        swal("Búsqueda fallida", "Elija un filtro, por favor", "./Imagenes/JigglypuffEnojado.png", {buttons: false, timer: 2500},)
         break
     }
     inputBuscador.value = ""
@@ -110,39 +95,55 @@ let limpiarBuscados = () => {
     ttitulo.innerHTML = ""
     ncontainer.innerHTML = ""
     ntitulo.innerHTML = ""
+    btnLimpiar.classList.add("hidden")
 }
 btnLimpiar.addEventListener("click", limpiarBuscados)
 
+let mostrarFiltradoNombre = () => {
+    let resultado = pokemones.filter(pokemon => pokemon.nombre.includes(inputBuscador.value.toLowerCase().trim()))
+    if (resultado.length > 0) {
+        resultado.forEach(x => {
+            if((buscadosNombre.some(y => y.nombre === x.nombre)) === false){
+                buscadosNombre.push(x)
+            }
+        })
+        sessionStorage.setItem("Buscados-Nombre", JSON.stringify(buscadosNombre))
+        container.innerHTML = `<img src="./Imagenes/EsperaPokeballNegroYBlancoNF.gif" class="imagen-carga">`
+        setTimeout(() => {
+            cargarPokemones(resultado)
+        }, parseInt(resultado.length)*350)
+        btnInteraccion = document.querySelectorAll("button.botonesEquipo")
+        mostrarBtnLimpiarStorage()
+    } else {
+        alertaBuscador(1)
+    }
+}
+
+let mostrarFiltradosTipo = () => {
+    let resultado = pokemones.filter(pokemon => pokemon.bTipo.includes(inputBuscador.value.toLowerCase().trim()))
+    if (resultado.length > 0) {
+        resultado.forEach(x => {
+            if((buscadosTipo.some(y => y.nombre === x.nombre)) === false) {
+                buscadosTipo.push(x)
+            }
+        })
+        sessionStorage.setItem("Buscados-Tipo", JSON.stringify(buscadosTipo))
+        container.innerHTML = `<img src="./Imagenes/EsperaPokeballNegroYBlancoNF.gif" class="imagen-carga">`
+        setTimeout(() => {
+            cargarPokemones(resultado)
+        }, parseInt(resultado.length)*350)
+        btnInteraccion = document.querySelectorAll("button.botonesEquipo")
+        mostrarBtnLimpiarStorage()
+    } else {
+        alertaBuscador(1)
+    }
+}
 
 let filtrarPokemonesNombre = () => {
         if (inputBuscador.value.trim() !== "" && opcionesFiltro.value === "nombre") {
-            let resultado = pokemones.filter(pokemon => pokemon.nombre.includes(inputBuscador.value.toLowerCase().trim()))
-            if (resultado.length > 0) {
-                resultado.forEach(x => {
-                    if((buscadosNombre.some(y => y.id === x.id)) === false){
-                        buscadosNombre.push(x)
-                    }
-                })
-                sessionStorage.setItem("Buscados-Nombre", JSON.stringify(buscadosNombre))
-                cargarPokemones(resultado)
-                btnInteraccion = document.querySelectorAll("button.botonesEquipo")
-            } else {
-                alertaBuscador(1)
-            }
+            mostrarFiltradoNombre()
         } else if (inputBuscador.value.trim() !== "" && opcionesFiltro.value === "bTipo") {
-            let resultado = pokemones.filter(pokemon => pokemon.bTipo.includes(inputBuscador.value.toLowerCase().trim()))
-            if (resultado.length > 0) {
-                resultado.forEach(x => {
-                    if((buscadosTipo.some(y => y.id === x.id)) === false) {
-                        buscadosTipo.push(x)
-                    }
-                })
-                sessionStorage.setItem("Buscados-Tipo", JSON.stringify(buscadosTipo))
-                cargarPokemones(resultado)
-                btnInteraccion = document.querySelectorAll("button.botonesEquipo")
-            } else {
-                alertaBuscador(1)
-            }
+            mostrarFiltradosTipo()
         } else if (opcionesFiltro.value === "vacio") {
             alertaBuscador(2)
             cargarPokemones(pokemones)
@@ -152,20 +153,41 @@ let filtrarPokemonesNombre = () => {
         }
         btnInteraccion = document.querySelectorAll("button.botonesEquipo")
         interaccionPokemon()
+        limpiarBuscados()
 }
 inputBuscador.addEventListener("search", filtrarPokemonesNombre)
 
 let mostrarBuscados = () => {
-    if ((sessionStorage.getItem("Buscados-Nombre")) !== null) {
-        let bnombre = JSON.parse(sessionStorage.getItem("Buscados-Nombre"))
-        cargarPokemonesBuscados(bnombre, ntitulo, ncontainer, "NOMBRE")
-    }
-    if ((sessionStorage.getItem("Buscados-Tipo")) !== null) {
-        let btipo = JSON.parse(sessionStorage.getItem("Buscados-Tipo"))
-        cargarPokemonesBuscados(btipo, ttitulo, tcontainer, "TIPO")
+    if((sessionStorage.getItem("Buscados-Nombre")) || (sessionStorage.getItem("Buscados-Tipo")) !== null) {
+        if ((sessionStorage.getItem("Buscados-Nombre")) !== null) {
+            let bnombre = JSON.parse(sessionStorage.getItem("Buscados-Nombre"))
+            cargarPokemonesBuscados(bnombre, ntitulo, ncontainer, "NOMBRE")
+        }
+        if ((sessionStorage.getItem("Buscados-Tipo")) !== null) {
+            let btipo = JSON.parse(sessionStorage.getItem("Buscados-Tipo"))
+            cargarPokemonesBuscados(btipo, ttitulo, tcontainer, "TIPO")
+        }
+    btnLimpiar.classList.remove("hidden")
+    } else {
+        swal("No hay historial", "Realice una búsqueda antes, por favor", "./Imagenes/noStorage.gif", {buttons: false, timer: 2500})
     }
 }
 btnBuscados.addEventListener("click", mostrarBuscados)
+
+let limpiarStorage = () => {
+    sessionStorage.clear("Buscados-Nombre")
+    sessionStorage.clear("Buscados-Tipo")
+    limpiarBuscados()
+    btnStorage.classList.add("hidden")
+}
+
+let mostrarBtnLimpiarStorage = () => {
+    if ((sessionStorage.getItem("Buscados-Nombre")) || (sessionStorage.getItem("Buscados-Tipo")) !== null) {
+        btnStorage.classList.remove("hidden")
+        btnStorage.addEventListener("click", limpiarStorage)
+    }
+}
+mostrarBtnLimpiarStorage()
 
 let interaccionPokemon = () => {
     btnInteraccion.forEach(btn => {
@@ -177,16 +199,16 @@ let interaccionPokemon = () => {
 }
 interaccionPokemon()
 
-
 let alertaMolestado = (x) => {
     swal("Acabás de molestar a " + capitalize(x.nombre), "", "./Imagenes/" + capitalize(x.nombre) + ".png", {
         buttons: {
           cancel: "¡Escapar!",
-          catch: {
-            text: "¡Arrojar Pokebola!",
-            value: "catch",},
-          defeat: true,},
-    }) .then((value) => {
+          catch: {text: "¡Arrojar Pokebola!",value: "catch"},
+          defeat: {text:"Derrotar",} 
+        },
+        }
+        )
+    .then((value) => {
         switch (value) {
           case "defeat":
             swal(capitalize(x.nombre) + " fue derrotado", "¡Ganaste 500XP!", "./Imagenes/PokemonDerrotado.gif");
